32860a9b361d12d2e9bc6e4a1fa34a27b7018004
alarconalvarez33-gif <alarconalvarez33@gmail.com>
2026-02-13 21:27:52 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -627,4 +627,20 @@ Code examples
       `}</style>
     </main>
   )
-}
+}Code examples
+<!-- modify this form HTML and place wherever you want your form -->
+<form
+  action="https://formspree.io/f/xreapnkb"
+  method="POST"
+>
+  <label>
+    Your email:
+    <input type="email" name="email">
+  </label>
+  <label>
+    Your message:
+    <textarea name="message"></textarea>
+  </label>
+  <!-- your other form fields go here -->
+  <button type="submit">Send</button>
+</form>
