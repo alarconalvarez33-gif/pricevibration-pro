f8feb56f238f48e283b78becdbe3d7d85e93f5ae
TheMentorTrading <contact@pricevibration.com>
2026-03-06 12:56:12 -0300

    fix: add CSP headers to allow TradingView scripts and iframes

--- next.config.js
+++ next.config.js
@@ -15,6 +15,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://s3.tradingview.com https://s.tradingview.com https://www.googletagmanager.com https://www.google-analytics.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: blob: https:",
+              "frame-src 'self' https://s.tradingview.com https://www.tradingview.com",
+              "connect-src 'self' https://s3.tradingview.com https://s.tradingview.com https://api.binance.com https://www.google-analytics.com",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
