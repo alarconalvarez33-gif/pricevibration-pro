9c9efba4034655560f5b6fe9f1424444fe9879f2
TheMentorTrading <contact@pricevibration.com>
2026-03-07 17:17:54 -0300

    fix: add fl=sv&fe=ci params to Fibonacci Vimeo embed URL

--- src/app/courses/fibonacci/page.tsx
+++ src/app/courses/fibonacci/page.tsx
@@ -75,7 +75,7 @@ export default async function FibonacciCoursePage() {
           <div className="relative w-full rounded-2xl overflow-hidden border border-[#c9a227]/30 shadow-2xl shadow-[#c9a227]/10">
             <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
               <iframe
-                src="https://player.vimeo.com/video/1171349347?h=a8a0610c95&badge=0&autopause=0&player_id=0&app_id=58479&sharing=0"
+                src="https://player.vimeo.com/video/1171349347?h=a8a0610c95&badge=0&autopause=0&player_id=0&app_id=58479&sharing=0&fl=sv&fe=ci"
                 allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
                 style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
                 title="Curso de Fibonacci"
