3c398c6264f6da62af60b61ba27fbe36ca7b9944
alarconalvarez33-gif <alarconalvarez33@gmail.com>
2026-05-02 08:36:48 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -349,7 +349,7 @@ export default function HomePage() {
                 <StatBubble
                   value="CEO "
                   label="TMT"
-                  position="-bottom-4 -left-3 sm:-left-6"
+                  position="-bottom-4 -left-6 sm:-left-8"
                 />
 
                 {/* Name tag below photo */}
